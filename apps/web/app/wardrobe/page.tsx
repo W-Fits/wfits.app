e50cc5fa6ce2168535ca0375e2346dc0284b5368
leomosley <leo.mosley2@gmail.com>
2025-03-23 17:03:43 +0000

    temp(apps/web/wardrobe): added nav buttons

--- apps/web/app/wardrobe/page.tsx
+++ apps/web/app/wardrobe/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function WardrobePage() {
   // https://v0.dev/chat/next-js-wardrobe-ui-KQ6Zi7PuMvn
   return (
@@ -5,6 +7,20 @@ export default function WardrobePage() {
       <header className="pt-12">
         <h1 className="text-3xl font-bold tracking-tight">My Wardrobe</h1>
       </header>
+      <div className="flex flex-col gap-1">
+        <Link
+          className="px-2 py-1 w-fit rounded-md bg-primary text-background"
+          href="/wardrobe/clothes"
+        >
+          Clothes
+        </Link>
+        <Link
+          className="px-2 py-1 w-fit rounded-md bg-primary text-background"
+          href="/wardrobe/outfits"
+        >
+          Outfits
+        </Link>
+      </div>
     </section>
   );
 }
