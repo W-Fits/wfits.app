81fe4b81c43223db9c2ed7e1b55db9c8da3df651
leomosley <leo.mosley2@gmail.com>
2025-03-25 12:56:11 +0000

    fix: user_id type

--- apps/web/next-auth.d.ts
+++ apps/web/next-auth.d.ts
@@ -3,7 +3,7 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string;
+      id: number;
       name: string;
       email: string;
       image: string;
@@ -11,7 +11,7 @@ declare module "next-auth" {
   }
 
   interface User {
-    user_id: string;
+    user_id: number;
     name: string;
     email: string;
     image: string;
@@ -20,7 +20,7 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string;
+    id: number;
     name: string;
     email: string;
     image: string;
